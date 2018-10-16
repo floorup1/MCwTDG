7a62cda93699d12f94a9536d0bb4ba1d05ac1b3a
Максим Гаранин <floorup@outlook.com>
2018-10-16 16:15:46 +0400

    Описал класс servorder, добавить массив?

---
+++ MCwTDG/Models/ServOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCwTDG.Models
+{
+
+    public class ServOrder
+    {
+        public long ServID { get; set; }
+        public string ServName { get; set; }
+        public string ServOGV { get; set; }
+    }
+}
--- MCwTDG/Views/DataGridPage.xaml.cs
+++ MCwTDG/Views/DataGridPage.xaml.cs
@@ -19,12 +19,12 @@ namespace MCwTDG.Views
             InitializeComponent();
         }
 
-        public ObservableCollection<SampleOrder> Source
+        public ObservableCollection<ServOrder> Source
         {
             get
             {
                 // TODO WTS: Replace this with your actual data
-                return SampleDataService.GetGridSampleData();
+                return ServDataService.GetGridServData();
             }
         }
 
