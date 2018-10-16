94d59b018f44403bfdac8a05191d57a0448bdac0
Максим Гаранин <floorup@outlook.com>
2018-10-16 09:59:33 +0400

    Добавьте файлы проекта.

---
+++ MCwTDG/Helpers/IPivotPage.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace MCwTDG.Helpers
+{
+    public interface IPivotPage
+    {
+        Task OnPivotSelectedAsync();
+
+        Task OnPivotUnselectedAsync();
+    }
+}
