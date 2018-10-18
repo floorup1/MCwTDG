a3bbd4f819cefe736b42d09f8ffa3e08b56246d8
Максим Гаранин <floorup@outlook.com>
2018-10-18 07:51:02 +0400

    base

---
+++ MCwTDG/Models/Information.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCwTDG.Models
+{
+    public class Information
+    {
+        public string InfoName { get; set; }
+        public string InfoText { get; set; }
+    }
+}
--- MCwTDG/Models/ServOrder.cs
+++ MCwTDG/Models/ServOrder.cs
@@ -8,5 +8,7 @@ namespace MCwTDG.Models
         public long ServID { get; set; }
         public string ServName { get; set; }
         public string ServOGV { get; set; }
+        public string ServTime { get; set; }
+        public string ServLevel { get; set; }
     }
 }
