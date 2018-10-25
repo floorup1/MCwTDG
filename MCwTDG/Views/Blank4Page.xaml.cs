706dd586264f0df660a220be2546b9276826eaf9
Максим Гаранин <floorup@outlook.com>
2018-10-25 09:53:23 +0400

    +обжалование +parsingevents

---
+++ MCwTDG/Views/Blank4Page.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Windows.UI.Xaml.Controls;
+
+namespace MCwTDG.Views
+{
+    public sealed partial class Blank4Page : Page, INotifyPropertyChanged
+    {
+        public Blank4Page()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- MCwTDG/Views/PageParseTablePage.xaml.cs
+++ MCwTDG/Views/PageParseTablePage.xaml.cs
@@ -31,7 +31,7 @@ namespace MCwTDG.Views
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-        private async void ContentArea_LoadingAsync(Windows.UI.Xaml.FrameworkElement sender, object args)
+        private async void ContentArea_TappedAsync(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
         {
             string html = @"http://mfc.ulgov.ru/index1.php?t=zagrujennost";
             var htmlDoc = new HtmlDocument();
@@ -61,6 +61,40 @@ namespace MCwTDG.Views
             TLenTime.Text = LenTime;
             TZheKol.Text = ZheKol;
             TZheTime.Text = ZheTime;
+        }
+
+        private async void ContentArea_DoubleTappedAsync(object sender, Windows.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
+        {
+            {
+                string html = @"http://mfc.ulgov.ru/index1.php?t=zagrujennost";
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.OptionReadEncoding = false;
+                var request = (HttpWebRequest)WebRequest.Create(html);
+                request.Method = "GET";
+                using (var response = (HttpWebResponse)await request.GetResponseAsync())
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        htmlDoc.Load(stream, Encoding.UTF8);
+                    }
+                }
+                string ZavKol = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='content rightPart']/div/table/tr[3]/td[2]/div").InnerText.Trim();
+                string ZavTime = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='content rightPart']/div/table/tr[3]/td[3]/div").InnerText.Trim();
+                string ZasKol = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='content rightPart']/div/table/tr[4]/td[2]/div").InnerText.Trim();
+                string ZasTime = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='content rightPart']/div/table/tr[4]/td[3]/div").InnerText.Trim();
+                string LenKol = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='content rightPart']/div/table/tr[5]/td[2]/div").InnerText.Trim();
+                string LenTime = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='content rightPart']/div/table/tr[5]/td[3]/div").InnerText.Trim();
+                string ZheKol = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='content rightPart']/div/table/tr[6]/td[2]/div").InnerText.Trim();
+                string ZheTime = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='content rightPart']/div/table/tr[6]/td[3]/div").InnerText.Trim();
+                TZavKol.Text = ZavKol;
+                TZavTime.Text = ZavTime;
+                TZasKol.Text = ZasKol;
+                TZasTime.Text = ZasTime;
+                TLenKol.Text = LenKol;
+                TLenTime.Text = LenTime;
+                TZheKol.Text = ZheKol;
+                TZheTime.Text = ZheTime;
+            }
         }
     }
 }
